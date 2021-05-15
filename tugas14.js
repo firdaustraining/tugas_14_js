37481dfafae0c320371bcd03dac5aeda78bb119a
Faza <firdausauliafaza@gmail.com>
2021-05-15 18:12:55 +0700

    niomic.com

---
+++ tugas14.js
@@ -0,0 +1,5 @@
+tanggallahir = 37 / 21;
+console.log(tanggallahir);
+console.log("round:", Math.round(tanggallahir));
+console.log("ceil:", Math.ceil(tanggallahir));
+console.log("floor:", Math.floor(tanggallahir));
